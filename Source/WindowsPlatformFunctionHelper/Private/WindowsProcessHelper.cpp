1a1d5e8bec3a92cbb7922663d506ac85347e3932
LorewalkerYu <lorewalkeryu@foxmail.com>
2020-04-20 16:59:09 +0800

    Create

---
+++ Source/WindowsPlatformFunctionHelper/Private/WindowsProcessHelper.cpp
@@ -0,0 +1,83 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WindowsProcessHelper.h"
+#include "Utils/WorkerThread.h"
+
+bool UWindowsProcessHelper::RequestToRunExe(const FString& ExcutablePath, const FString& CmdLineArgument, FGuid& OutGuid)
+{
+	const TCHAR* url = *ExcutablePath;
+	FProcHandle Handle = FPlatformProcess::CreateProc(url, *CmdLineArgument, true, false, false, nullptr, -1, nullptr, nullptr);
+	if (!Handle.IsValid())
+	{
+		return false;
+	}
+	FRunningProc Running(FGuid::NewGuid(), Handle);
+	RunningProcs.Add(Running);
+	if (RunningProcs.Num() == 1)
+	{
+		InnerThread = new FWorkerThread([this] { return CheckingProcRunning(); });
+		RunnableThread = FRunnableThread::Create(InnerThread, TEXT("WindowsProcRunningChecker"));
+	}
+	OutGuid = Running.Guid;
+	return true;
+}
+
+EProcRunningState UWindowsProcessHelper::IsProRunning(const FGuid& Guid)
+{
+	for (const FRunningProc& RunningProc : RunningProcs)
+	{
+		if (RunningProc.Guid == Guid)
+		{
+			FProcHandle Handle = RunningProc.Handle;
+			return (FPlatformProcess::IsProcRunning(Handle)) ? EProcRunningState::ERS_RUNNING : EProcRunningState::ERS_NULL;
+		}
+	}
+	return EProcRunningState::ERS_NULL;
+}
+
+int32 UWindowsProcessHelper::CurrentRunningProcNum()
+{
+	return RunningProcs.Num();
+}
+
+bool UWindowsProcessHelper::CheckingProcRunning()
+{
+	UE_LOG(LogTemp, Error, TEXT("CheckingProcRunning"));
+	TArray<FRunningProc> FinishedProc;
+	for (const FRunningProc& RunningProc : RunningProcs)
+	{
+		FProcHandle Handle = RunningProc.Handle;
+		if (!FPlatformProcess::IsProcRunning(Handle))
+		{
+			FinishedProc.Add(RunningProc);
+		}
+	}
+
+	for (const FRunningProc& Proc : FinishedProc)
+	{
+		RunningProcs.Remove(Proc);
+	}
+
+	return RunningProcs.Num() > 0;
+}
+
+void UWindowsProcessHelper::BeginDestroy()
+{
+	if (RunnableThread)
+	{
+		RunnableThread->Kill(true);
+		delete RunnableThread;
+		RunnableThread = nullptr;
+	}
+
+	if (InnerThread)
+	{
+		InnerThread->Stop();
+		delete InnerThread;
+		InnerThread = nullptr;
+	}
+	
+	Super::BeginDestroy();
+}
+
---
+++ Source/WindowsPlatformFunctionHelper/Public/WindowsProcessHelper.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "WindowsProcessHelper.generated.h"
+UENUM(BlueprintType)
+enum class EProcRunningState : uint8 
+{
+    ERS_NULL,
+    ERS_RUNNING
+};
+USTRUCT()
+struct FRunningProc
+{
+    GENERATED_USTRUCT_BODY()
+
+public:
+    FRunningProc() {};
+    FRunningProc(FGuid InGuid, FProcHandle InHandle) : Guid(InGuid), Handle(InHandle) {};
+    bool operator==(const FRunningProc& Other)
+    {
+        return Other.Guid == Guid;
+    }
+    FGuid Guid;
+    FProcHandle Handle; 
+
+};
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class WINDOWSPLATFORMFUNCTIONHELPER_API UWindowsProcessHelper : public UObject
+{
+
+	GENERATED_BODY()
+
+public:
+
+    UFUNCTION(BlueprintCallable, Category  = "WindowsProcessHelper|Excutable")
+        bool RequestToRunExe(const FString& ExcutablePath, const FString& CmdLineArgument, FGuid& OutGuid);
+
+	UFUNCTION(BlueprintCallable, Category = "WindowsProcessHelper|Excutable")
+        EProcRunningState IsProRunning(const FGuid& Guid);
+
+	UFUNCTION(BlueprintCallable, Category = "WindowsProcessHelper|Excutable")
+        int32 CurrentRunningProcNum();
+    bool CheckingProcRunning();
+
+    virtual void BeginDestroy() override;
+protected:
+
+    int32 NextIndex = 0;
+    TArray<FRunningProc> RunningProcs;
+
+	class FWorkerThread* InnerThread = nullptr;
+	class FRunnableThread* RunnableThread = nullptr;
+};
